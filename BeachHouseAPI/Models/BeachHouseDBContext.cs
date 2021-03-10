3d1aa50564c59f00c9d8f43e0b552d62c39695a2
Ale <aleamadorq@gmail.com>
2021-03-10 08:07:21 -0600

    Adding param table to models and context

--- BeachHouseAPI/Models/BeachHouseDBContext.cs
+++ BeachHouseAPI/Models/BeachHouseDBContext.cs
@@ -17,12 +17,14 @@ namespace BeachHouseAPI.Models
         {
         }
 
-        public virtual DbSet<User> User { get; set; }
+        public virtual DbSet<Param> Params { get; set; }
+        public virtual DbSet<User> Users { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
             {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
                 optionsBuilder.UseSqlServer("Server=CRH-LAP-106\\SQLEXPRESS;Database=BeachHouseDB;Trusted_Connection=True;");
             }
         }
@@ -30,6 +32,33 @@ namespace BeachHouseAPI.Models
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Param>(entity =>
+            {
+                entity.ToTable("Param");
+
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.Property(e => e.Description)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("description");
+
+                entity.Property(e => e.EndDate)
+                    .HasColumnType("datetime")
+                    .HasColumnName("end_date");
+
+                entity.Property(e => e.StartDate)
+                    .HasColumnType("datetime")
+                    .HasColumnName("start_date");
+
+                entity.Property(e => e.Value)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("value");
+            });
 
             modelBuilder.Entity<User>(entity =>
             {
---
+++ BeachHouseAPI/Models/Param.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace BeachHouseAPI.Models
+{
+    public partial class Param
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public string Value { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+    }
+}
