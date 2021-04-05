84de4507d9e2472fd2b5b03d8503cbd152d71f67
Ricardo <ricardo.bolanos@3pillarglobal.com>
2021-04-05 10:16:04 -0600

    CBH-52
    Consulta de usuarios, Consuslata de un solo usuario, y modificacion de usuario

---
+++ BeachHouseAPI/DTOs/UserEditDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeachHouseAPI.DTOs
+{
+    public class UserEditDTO:UserDTO
+    {
+        public string Id { get; set; }
+        public int Role { get; set; }
+        public bool Active { get; set; }
+
+    }
+}
